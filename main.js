7afa23a4cacd74f0bfca9057351d7b4542f328f5
Alexander Golin <agolin95@gmail.com>
2017-06-10 15:44:46 -0400

    Fix Twitter Button

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ $(function() {
     $('#emailButton').click(function() {
         window.location.href = ('mailto:eshapiro@eshap.tv');
     });
+    
+    $('#twitterButton').click(function() {
+       window.open("https://twitter.com/eshap?lang=en", "_blank")
+    });
 });
