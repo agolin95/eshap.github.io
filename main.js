838f1993df918d6ca84221ad393e16c4da2733bc
Alexander Golin <agolin95@gmail.com>
2017-08-25 23:51:02 -0400

    V2

--- main.js
+++ main.js
@@ -4,6 +4,21 @@ $(function() {
     });
     
     $('#twitterButton').click(function() {
-       window.open("https://twitter.com/eshap?lang=en", "_blank")
+       window.open("https://twitter.com/eshap?lang=en", "_blank");
     });
+	
+	$('#variety').hover(
+        function() {
+		    $(this).css("background-color", "#939BB4");
+          $(".readOn").css("color", "#B21414");
+	    },
+        function() {
+		    $(this).css("background-color", "#181E33");
+          $(".readOn").css("color", "#bbbbbb");
+	    }
+   );
+   
+   $('#variety').click(function() {
+      window.open("http://variety.com/2017/tv/news/evan-shapiro-troy-miller-eshap-tv-eat-your-words-pilot-1202537738/", '_blank');
+   });
 });
