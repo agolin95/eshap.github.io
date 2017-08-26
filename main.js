21c006429247c7847077ba210e4b284141a1719c
Alexander Golin <agolin95@gmail.com>
2017-08-26 16:08:31 -0400

    mobile fixes

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 $(function() {
-    $('#emailButton').click(function() {
+    $('.emailButton').click(function() {
         window.location.href = ('mailto:eshapiro@eshap.tv');
     });
     
-    $('#twitterButton').click(function() {
+    $('.twitterButton').click(function() {
        window.open("https://twitter.com/eshap?lang=en", "_blank");
     });
 	
