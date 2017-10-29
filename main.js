d951892c9c89ca244a80747041856e82eb2bf5c1
Alexander Golin <alexander@Alexanders-MacBook-Pro.local>
2017-10-29 19:17:56 -0400

    add sebastian article

--- main.js
+++ main.js
@@ -7,18 +7,21 @@ $(function() {
        window.open("https://twitter.com/eshap?lang=en", "_blank");
     });
 	
-	$('#variety').hover(
+	$('.articleBox').hover(
         function() {
 		    $(this).css("background-color", "#939BB4");
-          $(".readOn").css("color", "#B21414");
+          $(this).find(".readOn").css("color", "#B21414");
 	    },
         function() {
 		    $(this).css("background-color", "#181E33");
-          $(".readOn").css("color", "#bbbbbb");
+          $(this).find(".readOn").css("color", "#bbbbbb");
 	    }
    );
    
    $('#variety').click(function() {
       window.open("http://variety.com/2017/tv/news/evan-shapiro-troy-miller-eshap-tv-eat-your-words-pilot-1202537738/", '_blank');
+   });
+   $('#sebastian').click(function() {
+      window.open("http://variety.com/2017/tv/news/sebastian-wakes-up-pop-sebastian-conelli-pilot-order-1202599592/", '_blank');
    });
 });
